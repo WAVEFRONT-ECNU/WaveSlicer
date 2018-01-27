997fe91f6bd12f1d8b971ca7032ead1f7c642ad6
qcmiao1998 <qcmiao@hotmail.com>
2018-01-27 14:25:54 +0800

    delete unsuitable code

--- WaveSlicer/__init__.py
+++ WaveSlicer/__init__.py
@@ -1,16 +1,1 @@
-# -*- coding:UTF-8 -*-
-
-import WaveSlicer.speech_segmentation as seg
-
-frame_size = 256
-frame_shift = 128
-sr = 16000
-
-seg_point = seg.multi_segmentation("dialog4.wav",sr,frame_size,frame_shift,plot_seg=True)
-print(seg_point)
-
-
-
-
-
 
