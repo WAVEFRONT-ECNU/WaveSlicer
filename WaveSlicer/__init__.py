075c915ff49a0cdeb05a89adb9e65c55ddf2319a
qcmiao1998 <qcmiao@hotmail.com>
2018-01-30 22:44:19 +0800

    修改中间过程输出方法

--- WaveSlicer/__init__.py
+++ WaveSlicer/__init__.py
@@ -14,17 +14,20 @@ def cut_audio_fromfile(path, outpath, name):
 
 def cut_audio_fromstream(outpath, name):
     sr = 14400
-    savenum = 0
+    filenub = 0
     isfirst = True
     stream = import_stream.open_stream(sr)
     while True:
-        temp = import_stream.record_wav(stream, 10, sr)
+        temp = import_stream.record_wav(stream, 5, sr)
         if isfirst:
             y = temp
             isfirst = False
+        else:
+            y = y + temp
         segpoint = segmentation.multi_segmentation(y=y, sr=sr, frame_size=256, frame_shift=128,
                                                    is_only_have_voice=True)
-        save.save_wav_sequence(raw_y=y[:-1], sr=sr, segpoint=segpoint, path=outpath, name=name)
-        yt = int(segpoint[-1][1] * sr)
-        y = temp[yt:]
-        savenum += len(segpoint)
+        if len(segpoint) != 0:
+            save.save_wav_sequence(raw_y=y, sr=sr, segpoint=segpoint[:-1], path=outpath, name=name, startnum=filenub)
+            yt = int(segpoint[-1][0] * sr)
+            y = temp[yt:]
+        filenub += len(segpoint) - 1
