07c80ea846a9ba1aa8adbfc23779d4538b1426e3
qcmiao1998 <qcmiao@hotmail.com>
2018-02-02 22:17:05 +0800

    change to multi-threading

--- WaveSlicer/__init__.py
+++ WaveSlicer/__init__.py
@@ -3,6 +3,8 @@ import WaveSlicer.import_stream
 import WaveSlicer.segmentation
 import WaveSlicer.save
 import numpy as np
+import threading
+import librosa
 
 
 def cut_audio_fromfile(path, outpath, name):
@@ -14,20 +16,43 @@ def cut_audio_fromfile(path, outpath, name):
 
 def cut_audio_fromstream(outpath, name):
     sr = 16000
-    filenum = 0
-    isfirst = True
+    global __isfirst, temp, istempchanged, savepath, savename
+    savepath = outpath
+    savename = name
     stream = import_stream.open_stream(sr)
     while True:
         temp = import_stream.record_wav(stream, 5, sr)
-        if isfirst:
-            y = temp
-            isfirst = False
-        else:
-            y = np.append(y, temp)
-        segpoint = segmentation.multi_segmentation(y=y, sr=sr, frame_size=256, frame_shift=128,
-                                                   is_only_have_voice=True)
-        if len(segpoint) != 0:
-            save.save_wav_sequence(raw_y=y, sr=sr, segpoint=segpoint[:-1], path=outpath, name=name, startnum=filenum)
-            yt = int(segpoint[-1][0] * sr)
-            y = temp[yt:]
-        filenum += len(segpoint) - 1
+        tread = threading.Thread(target=__cutstream(temp))
+        tread.start()
+        savename += "a"
+
+
+__isfirst = True
+__filenum = 0
+y = np.ndarray
+sr = 16000
+savepath = ""
+savename = ""
+
+
+def __cutstream(temp):
+    global __isfirst, __filenum
+    global y, sr, savepath, savename
+    print("temp" + str(len(temp)))
+    if __isfirst:
+        y = temp
+        __isfirst = False
+    else:
+        y = np.append(y, temp)
+    print("B" + str(len(y)))
+    pt = savepath + str(__filenum) + ".wav"
+    librosa.output.write_wav(path=pt, y=y, sr=sr)
+    segpoint = segmentation.multi_segmentation(y=y, sr=sr, frame_size=256, frame_shift=128,
+                                               is_only_have_voice=True)
+    if len(segpoint) != 0:
+        save.save_wav_sequence(raw_y=y, sr=sr, segpoint=segpoint[:-1], path=savepath, name=savename, startnum=__filenum)
+        print(segpoint[-1][0])
+        yt = int(segpoint[-1][0] * sr)
+        y = temp[yt:]
+        print("A" + str(len(y)))
+        __filenum += len(segpoint) - 1
--- WaveSlicer/import_stream.py
+++ WaveSlicer/import_stream.py
@@ -3,14 +3,14 @@ import numpy as np
 
 
 def record_wav(stream, time, fs):
-    buffer_size = 1000
+    buffer_size = 6400
     save_buffer = b""
     read_time_per_second = fs / buffer_size
     cnt = 0
     print("Start Record")
     while cnt < time * read_time_per_second:
         str_data = stream.read(buffer_size)
-        print(str_data)
+        # print(str_data)
         save_buffer += str_data
         cnt += 1
     wave_data = np.frombuffer(save_buffer, dtype=np.float32)
@@ -19,7 +19,7 @@ def record_wav(stream, time, fs):
 
 def open_stream(fs):
     pa = pyaudio.PyAudio()
-    buffer_size = 1000
+    buffer_size = 6400
     stream = pa.open(
         format=pyaudio.paFloat32,
         channels=1,
